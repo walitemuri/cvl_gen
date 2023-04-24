25b78a7fe7c8d21eb0145f3a47e6e04576d42ace
walitemuri <wmtemuri@gmail.com>
2023-04-24 01:50:52 -0400

    Initial Commit

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+
+templates = Jinja2Templates(directory="./templates")
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/about")
+def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
